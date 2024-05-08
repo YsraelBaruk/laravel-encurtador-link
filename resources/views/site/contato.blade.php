dd182073a3d8239b53928f6baa6510f03a765124
israel <israekbaruque992@gmail.com>
2024-05-08 14:58:26 -0300

    form do contato

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -5,4 +5,34 @@
 @section('conteudo')
     @include('site.layouts._partials.menu')
     <h1>Contato</h1>
+
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <form action="{{route('site.contato')}}" method="GET">
+                    <div class="mb-3">
+                        <label for="nome" class="form-label">Nome Completo</label>
+                        <input type="text" class="form-control" id="nome" name="nome">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="mensagem" class="form-label">Mensagem</label>
+                        <input type="text" class="form-control" id="mensagem" name="mensagem">
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-select" id="motivo" name="motivo">
+                            <option selected>Qual o motivo de contato?</option>
+                            <option value="1">Reclamação</option>
+                            <option value="2">Dúvida</option>
+                            <option value="3">Elogio</option>
+                        </select>
+                    </div>
+                    <button type="button" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
