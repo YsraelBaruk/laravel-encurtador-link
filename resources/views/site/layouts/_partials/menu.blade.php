fcf9219ce8a78a116777dace3a8536d12d09119e
israel <israekbaruque992@gmail.com>
2024-05-08 15:01:03 -0300

    Fornecedores

--- resources/views/site/layouts/_partials/menu.blade.php
+++ resources/views/site/layouts/_partials/menu.blade.php
@@ -15,7 +15,7 @@
         <a href="{{ route('app.produtos') }}">Produtos</a>
     </li>
     <li>
-        <a href="{{ route('app.fornecedores') }}">fornecedores</a>
+        <a href="{{ route('app.fornecedores') }}">Fornecedores</a>
     </li>
     <li>
         <a href="{{ route('site.login') }}">Login</a>
