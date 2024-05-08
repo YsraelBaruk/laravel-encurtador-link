64626cea40e022fb736c7085364c6ccbebf4a2bf
israel <israekbaruque992@gmail.com>
2024-05-08 14:37:01 -0300

    usando o include

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,26 +1,4 @@
 @extends('site.layouts.basico')
 @section('conteudo')
-    <ul>
-        <li>
-            <a href="{{ route('site.principal') }}">Principal</a>
-        </li>
-        <li>
-            <a href="{{ route('site.sobrenos') }}">Sobre Nós</a>
-        </li>
-        <li>
-            <a href="{{ route('site.contato') }}">Contato</a>
-        </li>
-        <li>
-            <a href="{{ route('app.clientes') }}">Clientes</a>
-        </li>
-        <li>
-            <a href="{{ route('app.produtos') }}">Produtos</a>
-        </li>
-        <li>
-            <a href="{{ route('app.fornecedores') }}">fornecedores</a>
-        </li>
-        <li>
-            <a href="{{ route('site.login') }}">Login</a>
-        </li>
-    </ul>
+    @include('site.layouts._partials.menu')
 @endsection
