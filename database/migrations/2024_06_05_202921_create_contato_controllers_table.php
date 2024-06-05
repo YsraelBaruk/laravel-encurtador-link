45f9e133b2d0bb37da5f558ef1b40b8d8e1221c5
israel <israekbaruque992@gmail.com>
2024-06-05 17:58:05 -0300

    migration e conexao com o banco

--- database/migrations/2024_06_05_202921_create_contato_controllers_table.php
+++ database/migrations/2024_06_05_202921_create_contato_controllers_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contato_controllers', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('nome', 50);
+            $table->string('telefone', 20);
+            $table->string('email', 80);
+            $table->integer('motivo-contato');
+            $table->text('mensagem');
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contato_controllers');
     }
 };
